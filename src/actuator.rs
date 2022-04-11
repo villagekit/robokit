e67e9b918aa36abb5e4a47be2d287b9b1e59e8e3
Michael Williams <michael.williams@enspiral.com>
2022-04-11 18:59:51 +1200

    whatevs

--- src/actuator.rs
+++ src/actuator.rs
@@ -3,14 +3,11 @@ use fugit::HertzU32 as Hertz;
 use nb;
 use void;
 
-pub trait Waitable {
+pub trait Command<Message> {
     type Error;
 
+    fn command(&mut self, message: Message) -> ();
     fn wait(&mut self) -> nb::Result<(), Self::Error>;
-}
-
-pub trait Command<Message, Future> {
-    fn command(&mut self, message: Message) -> Future;
 }
 
 pub trait Listen<Event> {
@@ -30,42 +27,22 @@ pub struct LedBlink {
     duration: Hertz,
 }
 
-pub struct LedBlinkFuture<P, T>
+pub type LedError = void::Void;
+
+impl<P, T> Command<LedBlink> for Led<P, T>
 where
     P: OutputPin,
-    T: CountDown,
+    T: CountDown<Time = Hertz>,
 {
-    pin: P,
-    timer: T,
-}
+    fn command(&mut self, message: LedBlink) -> () {
+        self.timer.start(message.duration);
+    }
 
-pub struct LedError {}
-
-impl<P, T> Waitable for LedBlinkFuture<P, T>
-where
-    P: OutputPin,
-    T: CountDown,
-{
-    type Error = void::Void;
+    type Error = LedError;
 
     fn wait(&mut self) -> nb::Result<(), Self::Error> {
         self.pin.set_high().ok();
 
         self.timer.wait()
-    }
-}
-
-impl<P, T> Command<LedBlink, LedBlinkFuture<P, T>> for Led<P, T>
-where
-    P: OutputPin,
-    T: CountDown<Time = Hertz>,
-{
-    fn command(&mut self, message: LedBlink) -> LedBlinkFuture<P, T> {
-        self.timer.start(message.duration);
-
-        LedBlinkFuture {
-            pin: self.pin,
-            timer: self.timer,
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,14 @@ use gridbot;
 
 #[rtic::app(device = stm32f7xx_hal::pac, dispatchers = [USART1])]
 mod app {
-    use stm32f7xx_hal::{
-        gpio::{Output, PB0},
-        pac,
-        prelude::*,
-        timer::monotonic::MonoTimerUs,
-    };
+    use stm32f7xx_hal::{gpio::PB0, pac, prelude::*, timer::monotonic::MonoTimerUs};
 
     #[shared]
     struct Shared {}
 
     #[local]
     struct Local {
-        led: PB0<Output>,
+        led: gridbot::actuator::Led<PB0, pac::TIM3>,
     }
 
     #[monotonic(binds = TIM2, default = true)]
@@ -29,24 +24,24 @@ mod app {
         let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
 
         let gpiob = ctx.device.GPIOB.split();
-        let led = gpiob.pb0.into_push_pull_output();
-        tick::spawn().ok();
+        let led = gridbot::actuator::Led {
+            pin: gpiob.pb0.into_push_pull_output(),
+            timer: ctx.device.TIM3.monotonic_us(&clocks),
+        };
 
         let mono = ctx.device.TIM2.monotonic_us(&clocks);
         (Shared {}, Local { led }, init::Monotonics(mono))
     }
 
     #[idle]
-    fn idle(_: idle::Context) -> ! {
+    fn idle(ctx: idle::Context) -> ! {
         defmt::println!("Hello, world!");
 
-        loop {}
-    }
-
-    #[task(local = [led])]
-    fn tick(ctx: tick::Context) {
-        ctx.local.led.toggle();
-
-        tick::spawn_after(1.secs()).ok();
+        loop {
+            let led = ctx.local.led;
+            let command = gridbot::actuator::LedBlink { duration: 10.Hz() };
+            led.command(command);
+            nb::block!(led.wait());
+        }
     }
 }
