04ef50f9a7535a132172390cae0f71f7dfaa0115
Michael Williams <michael.williams@enspiral.com>
2022-04-14 01:36:14 +1200

    inspiration notes

--- src/actor.rs
+++ src/actor.rs
@@ -1,11 +1,13 @@
 use core::task::Poll;
 
+// ActorReceive trait inspired by https://github.com/rtic-rs/rfcs/pull/0052
 pub trait ActorReceive {
     type Message;
 
     fn receive(&mut self, message: &Self::Message);
 }
 
+// ActorPoll trait inspired by https://docs.rs/stepper
 pub trait ActorPoll {
     type Error;
 
