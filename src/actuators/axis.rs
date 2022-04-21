21f05e0b04ee4f99e50c4aaaa8742107d4b58401
Michael Williams <michael.williams@enspiral.com>
2022-04-21 17:45:06 +1200

    wip

--- src/actuators/axis.rs
+++ src/actuators/axis.rs
@@ -1,31 +1,41 @@
+use core::fmt::Debug;
 use core::marker::PhantomData;
 use core::task::Poll;
 use defmt::Format;
 use embedded_hal::digital::v2::OutputPin;
-use fixed;
-use fugit::MillisDurationU32 as MillisDuration;
-use fugit_timer::Timer;
+use embedded_hal::timer::CountDown;
+use embedded_time::duration::Nanoseconds;
+use fugit::TimerDurationU32;
+use ramp_maker;
 use stepper::{
-    drivers, motion_control, ramp_maker,
+    compat, drivers,
+    motion_control::{self, SoftwareMotionControl},
     traits::{
         EnableDirectionControl, EnableMotionControl, EnableStepControl, MotionControl,
         SetDirection, Step,
     },
     Direction, Error as StepperError, MoveToFuture, Stepper,
 };
-use typenum;
 
 use crate::actor::{ActorPoll, ActorReceive};
 
-pub struct Axis<Driver, PinDir, PinStep, T>
+type Driver<PinDir, PinStep, T> = SoftwareMotionControl<
+    drivers::dq542ma::DQ542MA<(), compat::Pin<PinDir>, compat::Pin<PinStep>>,
+    compat::Timer<T, 1_000_000>,
+    ramp_maker::Trapezoidal<f64>,
+    DelayToTicks,
+>;
+
+pub struct Axis<PinDir, PinStep, T>
 where
-    Driver: EnableDirectionControl<PinDir> + EnableStepControl<PinStep> + MotionControl,
     PinDir: OutputPin,
+    <PinDir as OutputPin>::Error: Debug,
     PinStep: OutputPin,
-    T: Timer<1_000_000>,
+    <PinStep as OutputPin>::Error: Debug,
+    T: CountDown,
 {
-    stepper: Stepper<Driver>,
-    move_to_future: Option<MoveToFuture<Driver>>,
+    stepper: Stepper<Driver<PinDir, PinStep, T>>,
+    move_to_future: Option<MoveToFuture<Driver<PinDir, PinStep, T>>>,
     real_position: f64,
     logical_position: f64,
     pin_dir: PhantomData<PinDir>,
@@ -33,42 +43,51 @@ where
     timer: PhantomData<T>,
 }
 
-type Num = fixed::FixedI64<typenum::U32>;
-
-impl<Driver, PinDir, PinStep, T> Axis<Driver, PinDir, PinStep, T>
+impl<PinDir, PinStep, T> Axis<PinDir, PinStep, T>
 where
-    Driver: EnableDirectionControl<PinDir> + EnableStepControl<PinStep> + MotionControl,
     PinDir: OutputPin,
     PinStep: OutputPin,
-    T: Timer<1_000_000>,
+    T: CountDown,
 {
-    pub fn new(dir: PinDir, step: PinStep, timer: T) -> Self {
-        let target_accel = Num::from_num(0.001); // steps / tick^2; 1000 steps / s^2
-        let max_speed = Num::from_num(0.001); // steps / tick; 1000 steps / s
+    pub fn new(dir: PinDir, step: PinStep, timer: T) -> Self
+    where
+        <PinDir as OutputPin>::Error: Debug,
+        <PinStep as OutputPin>::Error: Debug,
+    {
+        let target_accel = 0.001_f64; // steps / tick^2; 1000 steps / s^2
+        let max_speed = 0.001_f64; // steps / tick; 1000 steps / s
 
         let profile = ramp_maker::Trapezoidal::new(target_accel);
 
+        let compat_dir = compat::Pin(dir);
+        let compat_step = compat::Pin(step);
+        let compat_timer = compat::Timer(timer);
+
         let stepper = Stepper::from_driver(drivers::dq542ma::DQ542MA::new())
-            .enable_direction_control(dir, Direction::Forward, &mut timer)?
-            .enable_step_control(step)
-            .enable_motion_control((timer, profile, DelayToTicks));
+            .enable_direction_control(compat_dir, Direction::Forward, &mut compat_timer)
+            .unwrap()
+            .enable_step_control(compat_step)
+            .enable_motion_control((compat_timer, profile, DelayToTicks));
 
         Axis {
             stepper,
             move_to_future: None,
             real_position: 0.,
             logical_position: 0.,
+            pin_dir: PhantomData,
+            pin_step: PhantomData,
+            timer: PhantomData,
         }
     }
 }
 
 pub struct DelayToTicks;
-impl motion_control::DelayToTicks<Num> for DelayToTicks {
-    type Ticks = MillisDuration;
+impl motion_control::DelayToTicks<f64> for DelayToTicks {
+    type Ticks = Nanoseconds;
     type Error = core::convert::Infallible;
 
-    fn delay_to_ticks(&self, delay: Num) -> Result<Self::Ticks, Self::Error> {
-        Ok(MillisDuration::from_ticks(delay.int()))
+    fn delay_to_ticks(&self, delay: f64) -> Result<Self::Ticks, Self::Error> {
+        Ok(Nanoseconds(delay.to_num::<u32>()))
     }
 }
 
@@ -77,12 +96,11 @@ pub struct AxisMoveMessage {
     pub distance_in_millimeters: f64,
 }
 
-impl<Driver, PinDir, PinStep, T> ActorReceive for Axis<Driver, PinDir, PinStep, T>
+impl<PinDir, PinStep, T> ActorReceive for Axis<PinDir, PinStep, T>
 where
-    Driver: EnableDirectionControl<PinDir> + EnableStepControl<PinStep> + MotionControl,
     PinDir: OutputPin,
     PinStep: OutputPin,
-    T: Timer<1_000_000>,
+    T: CountDown,
 {
     type Message = AxisMoveMessage;
 
@@ -100,12 +118,11 @@ pub enum AxisError {
     Stepper,
 }
 
-impl<Driver, PinDir, PinStep, T> ActorPoll for Axis<Driver, PinDir, PinStep, T>
+impl<PinDir, PinStep, T> ActorPoll for Axis<PinDir, PinStep, T>
 where
-    Driver: EnableDirectionControl<PinDir> + EnableStepControl<PinStep> + MotionControl,
     PinDir: OutputPin,
     PinStep: OutputPin,
-    T: Timer<1_000_000>,
+    T: CountDown,
 {
     type Error = AxisError;
 
