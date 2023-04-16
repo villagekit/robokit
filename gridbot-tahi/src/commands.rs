5f71470eb3655a9c8c14693b8ebf3e73df21e2af
Mikey <michael.williams@enspiral.com>
2023-04-17 11:50:24 +1200

    use fixed_map as better actuator registry (#38)

--- gridbot-tahi/src/commands.rs
+++ gridbot-tahi/src/commands.rs
@@ -1,58 +1,77 @@
+use defmt::Format;
+use fixed_map::Key;
 use fugit::ExtU32;
 use robokit::{
     actuators::{axis::AxisAction, led::LedAction},
     runner::Command,
 };
 
-type BotCommand<const TIMER_HZ: u32> = Command<'static, TIMER_HZ>;
+#[derive(Copy, Clone, Debug, Format, Key)]
+pub enum LedId {
+    Green,
+    Blue,
+    Red,
+}
+
+#[derive(Copy, Clone, Debug, Format, Key)]
+pub enum AxisId {
+    X,
+}
+
+#[derive(Copy, Clone, Debug, Format, Key)]
+pub enum SpindleId {
+    Main,
+}
+
+type BotCommand<const TIMER_HZ: u32> = Command<TIMER_HZ, LedId, AxisId, SpindleId>;
 
 pub fn get_run_commands<const TIMER_HZ: u32>() -> [BotCommand<TIMER_HZ>; 8] {
     [
         Command::Led(
-            "green",
+            LedId::Green,
             LedAction::Blink {
                 duration: 50.millis(),
             },
         ),
         Command::Led(
-            "blue",
+            LedId::Blue,
             LedAction::Blink {
                 duration: 100.millis(),
             },
         ),
         Command::Led(
-            "red",
+            LedId::Red,
             LedAction::Blink {
                 duration: 200.millis(),
             },
         ),
         Command::Axis(
-            "axis",
+            AxisId::X,
             AxisAction::MoveRelative {
                 max_velocity_in_millimeters_per_sec: 10_f64,
                 distance_in_millimeters: 40_f64,
             },
         ),
         Command::Led(
-            "red",
+            LedId::Red,
             LedAction::Blink {
                 duration: 50.millis(),
             },
         ),
         Command::Led(
-            "blue",
+            LedId::Blue,
             LedAction::Blink {
                 duration: 100.millis(),
             },
         ),
         Command::Led(
-            "green",
+            LedId::Green,
             LedAction::Blink {
                 duration: 200.millis(),
             },
         ),
         Command::Axis(
-            "x",
+            AxisId::X,
             AxisAction::MoveRelative {
                 max_velocity_in_millimeters_per_sec: 10_f64,
                 distance_in_millimeters: -40_f64,
@@ -64,7 +83,7 @@ pub fn get_run_commands<const TIMER_HZ: u32>() -> [BotCommand<TIMER_HZ>; 8] {
 pub fn get_start_commands<const TIMER_HZ: u32>() -> [BotCommand<TIMER_HZ>; 1] {
     [
         Command::Axis(
-            "x",
+            AxisId::X,
             AxisAction::Home {
                 max_velocity_in_millimeters_per_sec: 10_f64,
                 back_off_distance_in_millimeters: 0.1_f64,
@@ -81,7 +100,7 @@ pub fn get_start_commands<const TIMER_HZ: u32>() -> [BotCommand<TIMER_HZ>; 1] {
 pub fn get_stop_commands<const TIMER_HZ: u32>() -> [BotCommand<TIMER_HZ>; 1] {
     [
         Command::Axis(
-            "x",
+            AxisId::X,
             AxisAction::MoveAbsolute {
                 max_velocity_in_millimeters_per_sec: 10_f64,
                 position_in_millimeters: 0_f64,
